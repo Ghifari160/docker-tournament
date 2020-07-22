34202a7346990bdc2ee5f2fd5229eae42a83bdc5
GHIFARI160 <ghifari@ghifari160.com>
2020-07-21 20:34:21 -0500

    Update to Tournament v0.3.0
    Tournament has been updated to v0.3.0. This update is backwards
    compatible with v0.2.0.

--- tournament-setup.sh
+++ tournament-setup.sh
@@ -3,7 +3,7 @@
 tournament_setup() {
     chown node:node /var/www/tournament
     chmod 755 /var/www/tournament
-    curl -sL https://github.com/Ghifari160/tournament/releases/download/v0.2.0/up-2020062401-v0.2.0.tar.gz | tar -zxvf - -C /var/www/tournament
+    curl -sL https://github.com/Ghifari160/tournament/releases/download/v0.3.0/tournament-v0.3.0.tar.gz | tar -zxvf - -C /var/www/tournament --overwrite
     cd /var/www/tournament
     npm i --only=prod
 
@@ -11,15 +11,15 @@ tournament_setup() {
     rm -rv /tmp/v8*
 }
 
-if [ ! -f /init/tournament-v0.2.0 ]; then
+if [ ! -f /init/tournament-v0.3.0 ]; then
     tournament_setup
-    touch /init/tournament-v0.2.0
+    touch /init/tournament-v0.3.0
 
     if [ -f /init/tournament ]; then
         rm /init/tournament
     fi
 
-    ln -s /init/tournament-v0.2.0 /init/tournament
+    ln -s /init/tournament-v0.3.0 /init/tournament
 else
     sleep 2
 fi
