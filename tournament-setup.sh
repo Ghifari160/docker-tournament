84c2656ddf58725bfd98192255965a4d2926b70a
GHIFARI160 <ghifari@ghifari160.com>
2020-08-16 18:32:47 -0500

    Update to Tournament v0.3.1
    Tournament has been updated to v0.3.1, which introduces some quality of life features and mitigates CVE-2020-7660.

--- tournament-setup.sh
+++ tournament-setup.sh
@@ -3,7 +3,7 @@
 tournament_setup() {
     chown node:node /var/www/tournament
     chmod 755 /var/www/tournament
-    curl -sL https://github.com/Ghifari160/tournament/releases/download/v0.3.0/tournament-v0.3.0.tar.gz | tar -zxvf - -C /var/www/tournament --overwrite
+    curl -sL https://github.com/Ghifari160/tournament/releases/download/v0.3.1/tournament-v0.3.1.tar.gz | tar -zxvf - -C /var/www/tournament --overwrite
     cd /var/www/tournament
     npm i --only=prod
 
@@ -11,15 +11,15 @@ tournament_setup() {
     rm -rv /tmp/v8*
 }
 
-if [ ! -f /init/tournament-v0.3.0 ]; then
+if [ ! -f /init/tournament-v0.3.1 ]; then
     tournament_setup
-    touch /init/tournament-v0.3.0
+    touch /init/tournament-v0.3.1
 
     if [ -f /init/tournament ]; then
         rm /init/tournament
     fi
 
-    ln -s /init/tournament-v0.3.0 /init/tournament
+    ln -s /init/tournament-v0.3.1 /init/tournament
 else
     sleep 2
 fi
